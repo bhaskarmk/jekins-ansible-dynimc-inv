116407526c5ba1689876b2dda0858792645b4f2b
bhaskar <119286789+bhaskarmk@users.noreply.github.com>
2023-01-23 20:45:34 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,7 +3,6 @@ provider "aws"{
 }
 
 resource "aws_instance" "AWSEC2Instance"{
-     count   = 1
      ami = "ami-0f9d9a251c1a44858"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
